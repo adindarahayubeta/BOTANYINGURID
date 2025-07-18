9491bcba51ca5b6233f9a5bff858198474275dce
adindarahayubeta <adindarahayubeta@gmail.com>
2025-07-18 11:56:20 +0700

    Update image_urls.py

--- image_urls.py
+++ image_urls.py
@@ -1,5 +1,5 @@
 IMAGE_URLS = [
-    "https://example.com/image1.jpg",
-    "https://example.com/image2.png",
-    "https://example.com/image3.jpg",
+    "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhK-HMD7B_JC9Z0PTfldD9n_mpd_fb1995f62-9y99tQ0M-RXBwCc3nn5ezh0sh1QkGlpE_Alm2q5Vbcy6puEQfcOHcBKXNFObuoPxCcyAF-y41LAJdhtB0NJmQ6faJlhElU_LkM7GY8ng0f1AxJb7Rj1_Lb8MJhbDO6ERIDZgY3UB__XhDOB-VpDC9ky59/s718/Screenshot_110.png",
+    "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhK-HMD7B_JC9Z0PTfldD9n_mpd_fb1995f62-9y99tQ0M-RXBwCc3nn5ezh0sh1QkGlpE_Alm2q5Vbcy6puEQfcOHcBKXNFObuoPxCcyAF-y41LAJdhtB0NJmQ6faJlhElU_LkM7GY8ng0f1AxJb7Rj1_Lb8MJhbDO6ERIDZgY3UB__XhDOB-VpDC9ky59/s718/Screenshot_110.png",
+    "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhK-HMD7B_JC9Z0PTfldD9n_mpd_fb1995f62-9y99tQ0M-RXBwCc3nn5ezh0sh1QkGlpE_Alm2q5Vbcy6puEQfcOHcBKXNFObuoPxCcyAF-y41LAJdhtB0NJmQ6faJlhElU_LkM7GY8ng0f1AxJb7Rj1_Lb8MJhbDO6ERIDZgY3UB__XhDOB-VpDC9ky59/s718/Screenshot_110.png",
 ]
